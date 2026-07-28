87baa79338efea0b0a54b578af6cfa84c33c912e
akadmiri <kadmiri10@outlook.com>
2026-07-29 00:36:06 +0100

    Evaluate the SVM model.

--- src/train.py
+++ src/train.py
@@ -64,7 +64,7 @@ def train_svm(X_train, y_train):
     scaler = StandardScaler()
     X_train_scaled = scaler.fit_transform(X_train)
     model = SVC(kernel="linear", class_weight="balanced", random_state=10)
-    model.fit(X_train, y_train)
+    model.fit(X_train_scaled, y_train)
     return model, scaler
 
 def evaluate_svm(model, scaler, X_test, y_test):
@@ -80,20 +80,29 @@ def evaluate_svm(model, scaler, X_test, y_test):
 def compare_models(X_train, y_train, n_splits=5):
     cv = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=10)
 
+    # Logistic Regression    
     logreg = LogisticRegression(class_weight="balanced", max_iter=1000, random_state=10)
     scaler = StandardScaler()
     X_train_scaled = scaler.fit_transform(X_train)
     logreg_scores = cross_val_score(logreg, X_train_scaled, y_train, cv=cv, scoring="average_precision")
 
     scale_pos_weight = (y_train == 0).sum() / (y_train == 1).sum()
+
+    # XGB tree
     xgb = XGBClassifier(
         n_estimators=300, max_depth=4, learning_rate=0.05,
         scale_pos_weight=scale_pos_weight, eval_metric="aucpr", random_state=10,
     )
     xgb_scores = cross_val_score(xgb, X_train, y_train, cv=cv, scoring="average_precision")
 
+    # SVM 
+    svm = SVC(kernel="linear", class_weight="balanced", random_state=10)
+    svm_scores = cross_val_score(svm, X_train_scaled, y_train, cv=cv, scoring="average_precision")
+
+
     print(f"Logistic Regression PR-AUC: {logreg_scores.mean():.3f} ± {logreg_scores.std():.3f}")
     print(f"XGBoost PR-AUC:              {xgb_scores.mean():.3f} ± {xgb_scores.std():.3f}")
+    print(f"SVM PR-AUC: {svm_scores.mean():.3f} ± {svm_scores.std():.3f}")
 
 
 if __name__ == "__main__":
@@ -110,7 +119,12 @@ if __name__ == "__main__":
     xgb_model = train_xgboost(X_train, y_train)
     evaluate_xgb(xgb_model, X_test, y_test)
 
+    print("\nSVM: ")
+    svm_model, svm_scaler = train_svm(X_train, y_train)
+    evaluate_svm(svm_model, svm_scaler, X_test, y_test)
+
+'''
     joblib.dump(model, "output/model.joblib" )
     joblib.dump(scaler, "output/scaler.joblib")
     joblib.dump(xgb_model, "output/xgbmodel.joblib")
-
+'''
